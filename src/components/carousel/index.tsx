eccfd2a1758a5a77941048a8a59a4904fbd543a4
odiljonswe <karimovodilbek008@gmail.com>
2024-04-07 00:42:41 +0500

    update

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,18 +1,17 @@
+import CarouselImg1 from "../../assets/carouselImg.jpg";
+import CarouselImg2 from "../../assets/carouselImg2.jpg";
+import CarouselImg3 from "../../assets/carouselImg3.jpg";
 import { Carousel } from "flowbite-react";
-
-
 const CarouselUI = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      <Carousel className="md:h-[500px]">
+        <img src={CarouselImg1} alt="..." />
+        <img src={CarouselImg2} alt="..." />
+        <img src={CarouselImg3} alt="..." />
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
 export default CarouselUI;
