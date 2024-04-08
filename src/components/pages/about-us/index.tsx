c9a759033c1485ed07f12ceb2926b2f6a3c3d40b
odiljonswe <karimovodilbek008@gmail.com>
2024-04-08 11:46:06 +0500

    update

--- src/components/pages/about-us/index.tsx
+++ src/components/pages/about-us/index.tsx
@@ -1,68 +1,89 @@
 import Group from "../../../assets/group-of.png";
+import Footer from "../../footer";
 const About = () => {
   return (
     <div className="container-box">
-      <div className="flex">
-        <div>
-          <h2>Buxoro tabiiy mahsuloti</h2>
-          <h2 className="">
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-            sumkalarFabrikamız ko'p yillar davomida butun dunyoda foydalanish
-            uchun paxta matolarini ishlab chiqaradigan kompaniya bo'lib kelgan.
+      <div className="flex gap-[80px] max-md:flex-col-reverse max-md:gap-[20px]">
+        <div className="max-w-[580px] w-full gap-8 flex flex-col">
+          <h2 className="text-[50px] font-bold max-md:text-[25px]">
+            Buxoro tabiiy mahsuloti
           </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarUshbu
-            kompaniya 17 yildan beri mijozlarga xizmat ko'rsatmoqda.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarUshbu
-            kompaniyaning asosiy maqsadi yuqori sifatli va tejamkor mahsulotlar
-            ishlab chiqarishdir.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarUshbu
-            kompaniyaning ishlab chiqarish jarayoni butunlay ekologik toza.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-            sumkalarKorxonada xaridorlarning talab va takliflari asosida har
-            qanday turdagi paxta xomashyosi ishlab chiqarilishi mumkin.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-            sumkalarKompaniya DongJia to‘quv mashinalarining O‘zbekistondagi
-            rasmiy dileri hisoblanadi.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarAyni
-            paytda 80 dan ortiq oila o‘z oilasini moddiy jihatdan ta’minlab,
-            korxona nufuzi va muvaffaqiyatiga hissa qo‘shmoqda.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarBu
-            talabni qondirish uchun 50 ta to‘quv dastgohi to‘xtovsiz yuqori
-            tezlikda ishlamoqda.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarOyiga
-            200-250 ming metr gazlama to‘qish quvvatiga egamiz.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-            sumkalarMijozlarning yuqori talabidan kelib chiqib, korxonada yuqori
-            sifatli yuqori sifatli eko-sumkalar ishlab chiqarish yo‘lga
-            qo‘yildi.
-          </h2>
-          <h2>
-            Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalar“Bukhara
-            Natural Product” kompaniyasi oʻz sodiq mijozlariga istalgan vaqtda
-            xizmat koʻrsatishdan mamnun.
-          </h2>
+          <div className="flex flex-col gap-[50px] max-md:gap-[20px]">
+            <h2 className="text-[18px] max-md:text-[14px]">
+              Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+              sumkalarFabrikamız ko'p yillar davomida butun dunyoda foydalanish
+              uchun paxta matolarini ishlab chiqaradigan kompaniya bo'lib
+              kelgan.
+            </h2>
+            <h2 className="text-[18px] max-md:text-[14px]">
+              Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarUshbu
+              kompaniya 17 yildan beri mijozlarga xizmat ko'rsatmoqda.
+            </h2>
+            <span className="max-lg:hidden max-md:block">
+              <div className="flex flex-col gap-[50px] max-md:gap-[20px]">
+
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarUshbu kompaniyaning asosiy maqsadi yuqori sifatli va
+                tejamkor mahsulotlar ishlab chiqarishdir.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarUshbu kompaniyaning ishlab chiqarish jarayoni butunlay
+                ekologik toza.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarKorxonada xaridorlarning talab va takliflari asosida har
+                qanday turdagi paxta xomashyosi ishlab chiqarilishi mumkin.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarKompaniya DongJia to‘quv mashinalarining O‘zbekistondagi
+                rasmiy dileri hisoblanadi.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarAyni
+                paytda 80 dan ortiq oila o‘z oilasini moddiy jihatdan ta’minlab,
+                korxona nufuzi va muvaffaqiyatiga hissa qo‘shmoqda.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalarBu
+                talabni qondirish uchun 50 ta to‘quv dastgohi to‘xtovsiz yuqori
+                tezlikda ishlamoqda.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarOyiga 200-250 ming metr gazlama to‘qish quvvatiga
+                egamiz.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalarMijozlarning yuqori talabidan kelib chiqib, korxonada
+                yuqori sifatli yuqori sifatli eko-sumkalar ishlab chiqarish
+                yo‘lga qo‘yildi.
+              </h2>
+              <h2 className="text-[18px] max-md:text-[14px]">
+                Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
+                sumkalar“Bukhara Natural Product” kompaniyasi oʻz sodiq
+                mijozlariga istalgan vaqtda xizmat koʻrsatishdan mamnun.
+              </h2>
+              </div>
+
+            </span>
+          </div>
         </div>
-        <div>
-          <img src={Group} alt="" />
+        <div className="flex items-center justify-center">
+          <img
+            className="rounded-3xl"
+            width={850}
+            height={635}
+            src={Group}
+            alt=""
+          />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
