de6e97e4c5f7f098a0913c400a2057bf5653c684
odiljonswe <karimovodilbek008@gmail.com>
2024-04-09 15:01:10 +0500

    update

---
+++ src/components/data/index.tsx
@@ -0,0 +1,17 @@
+const Collection = [
+  {
+    id: 1,
+    name: "Alpari",
+    img: "assets/img1.png",
+  },
+  {
+    id: 2,
+    name: "Arabesque",
+    img: "assets/img2.png",
+  },
+  {
+    id: 3,
+    name: "ba,bi",
+    img: "assets/img3   .png",
+  },
+];
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,11 +1,49 @@
 import CarouselUI from "../../carousel";
+import Icon1 from "../../../assets/noun_delivery.svg";
+import Icon2 from "../../../assets/noun_guarantee.svg";
+import Icon3 from "../../../assets/noun_airpods.svg";
+import Icon4 from "../../../assets/noun_Wallet.svg";
 
 const Home = () => {
-  return <div>
+  return (
     <div className="container-box">
-      <CarouselUI />
+      <div>
+        <CarouselUI />
+      </div>
+      <div>
+        <ul className="flex mt-[200px] items-center justify-between">
+          <li className="flex gap-7">
+            <img src={Icon1} alt="icon" />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Free shipping</h2>
+              <p className="text-[#AAAAAA]">Free shipping on all orders</p>
+            </div>
+          </li>
+          <li className="flex gap-7">
+            <img src={Icon2} alt="icon" />
+            <div className="flex flex-col">
+              <h2  className="font-bold">Return Guarantee</h2>
+              <p className="text-[#AAAAAA]">30 day money back</p>
+            </div>
+          </li>
+          <li className="flex gap-7">
+            <img src={Icon3} alt="icon" />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Online support 24/7</h2>
+              <p className="text-[#AAAAAA]">Technical support 24/7</p>
+            </div>
+          </li>
+          <li className="flex gap-7">
+            <img src={Icon4} alt="icon" />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Secure payment</h2>
+              <p className="text-[#AAAAAA]">All payment methods accepted</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default Home;
