ce8bbbabd52fa0dede0f33f8eb3f3810918f5461
odiljonswe <karimovodilbek008@gmail.com>
2024-04-11 15:54:42 +0500

    update

--- src/components/data/index.tsx
+++ src/components/data/index.tsx
@@ -10,6 +10,16 @@ import img9 from "../../assets/img9.jpg";
 import img10 from "../../assets/img10.jpg";
 import img11 from "../../assets/img11.jpg";
 import img12 from "../../assets/img12.jpg";
+import img13 from "../../assets/img13.jpg";
+import img14 from "../../assets/img14.jpg";
+import img15 from "../../assets/img15.jpg";
+import img16 from "../../assets/img16.jpg";
+import img17 from "../../assets/img17.jpg";
+import img18 from "../../assets/img18.jpg";
+import img19 from "../../assets/img19.jpg";
+import img20 from "../../assets/img20.jpg";
+import img21 from "../../assets/img21.jpg";
+import img22 from "../../assets/img22.jpg";
 const Collection = [
   {
     id: 1,
@@ -70,6 +80,46 @@ const Collection = [
     id: 12,
     name: "Vasilisa",
     img: img12,
+  },  {
+    id: 13,
+    name: "Vasilisa",
+    img: img13,
+  },  {
+    id: 14,
+    name: "Vasilisa",
+    img: img14,
+  },  {
+    id: 15,
+    name: "Vasilisa",
+    img: img15,
+  },  {
+    id: 16,
+    name: "Vasilisa",
+    img: img16,
+  },  {
+    id: 17,
+    name: "Vasilisa",
+    img: img17,
+  },  {
+    id: 18,
+    name: "Vasilisa",
+    img: img18,
+  },  {
+    id: 19,
+    name: "Vasilisa",
+    img: img19,
+  },  {
+    id: 20,
+    name: "Vasilisa",
+    img: img20,
+  },  {
+    id: 21,
+    name: "Vasilisa",
+    img: img21,
+  },  {
+    id: 22,
+    name: "Vasilisa",
+    img: img22,
   },
 ];
 export default Collection;
